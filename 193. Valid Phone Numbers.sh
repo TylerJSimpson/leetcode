c76117284e80d39eee8f90eaf01796ec6bc319ad
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-01-11 14:56:48 -0500

    Create 193. Valid Phone Numbers.sh

---
+++ 193. Valid Phone Numbers.sh
@@ -0,0 +1,15 @@
+#Bash script to check for valid phone number
+#Valid: XXX-XXX-XXXX or (XXX) XXX-XXXX
+
+grep -E '^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$' file.txt
+
+:'
+^ start line
+$ end line
+\ escape trailing character
+() grouping
+[0-9]{3}-[0-9]{3}-[0-9]{4}$
+XXX-XXX-XXXX
+([0-9]{3}\) [0-9]{3}-[0-9]{4}
+(XXX) XXX-XXXX
+'
