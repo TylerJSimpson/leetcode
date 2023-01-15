90471bbab78fbf790643d5956913f4b99c955566
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-01-15 09:14:17 -0500

    Update 121. Best Time to Buy and Sell Stock.py

--- python/easy/121. Best Time to Buy and Sell Stock.py
+++ python/easy/121. Best Time to Buy and Sell Stock.py
@@ -1,14 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
-        max_price = prices[0]
+        """
+        maxProfit returns difference in prices[i] where maximum profit can be obtained 
+            returns 0 if no profit can be made
+        prices array (1 <= prices.length <= 105), (0 <= prices[i] <= 104)
+            list of prices on consecutive days
+        """
+        min_price = prices[0]                                   #set min_price to 1st value in prices    
+        max_price = prices[0]                                   #set max_price ot 1st value in prices
         diff_price = 0
-        for price in prices:
-            if price < min_price:
-                min_price = price
-                max_price = price
+        for price in prices:                                    
+            if price < min_price:                               #iterate through prices
+                min_price = price                               #assign lowest value in prices to min_price
+                max_price = price                               #assign lowest value in prices to max_price
             elif price > max_price:
-                max_price = price
+                max_price = price                               #assign highest value in prices to max_price
                 if diff_price < max_price - min_price:
-                    diff_price = max_price - min_price
+                    diff_price = max_price - min_price          #calculate diff_price
         return diff_price
