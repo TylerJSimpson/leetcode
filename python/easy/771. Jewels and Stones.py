6443f96f57f9d67f0043374142d42d0bd4b8b51e
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-01 08:36:11 -0500

    Update 771. Jewels and Stones.py

--- python/easy/771. Jewels and Stones.py
+++ python/easy/771. Jewels and Stones.py
@@ -1,10 +1,10 @@
 class Solution:
-    ```
-    numJewelsInStone counts the number of jewels that are also stones
-    jewels: string
-    stones: string
-    ```
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        ```
+        numJewelsInStone counts the number of jewels that are also stones
+        jewels: string
+        stones: string
+        ```
         jewels_list = list(jewels)
         stones_list = list(stones)
         jewel_count = 0
