4b36935310add270d4ac9dd280d8a1d1d258bfa3
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-07 18:25:02 -0500

    Update 1929. Concatenation of Array.py

--- python/easy/1929. Concatenation of Array.py
+++ python/easy/1929. Concatenation of Array.py
@@ -1,8 +1,12 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        nums_length = len(nums) * 2
+    '''
+    getConcatenation takes an input array nums and concatenates it with itself
+    nums: int array (n == nums.length, 1 <= n <= 1000, 1 <= nums[i] <= 1000)
+    '''
+        nums_length = len(nums) * 2                 #double the length of array nums
         nums_concat = []
-        while len(nums_concat) < nums_length:
-            for i in range(len(nums)):
+        while len(nums_concat) < nums_length:       #iterate through array nums twice
+            for i in range(len(nums)):              #add each character to new array
                 nums_concat.append(nums[i])
         return nums_concat
