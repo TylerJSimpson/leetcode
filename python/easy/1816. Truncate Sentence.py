cccef91dd02991dfc0f911bc55c4858561eb6c05
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-07 18:15:53 -0500

    Update 1816. Truncate Sentence.py

--- python/easy/1816. Truncate Sentence.py
+++ python/easy/1816. Truncate Sentence.py
@@ -1,11 +1,16 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        truncated_string = ''
+    '''
+    truncateSentence takes input string s and includes only the first k (int) words
+    s: str (only lowercase and uppercase English letters and spaces, 1 <= s.length <= 500, words separated only by a single space, no leading/trailing spaces)
+    k: int (range [1, number of words in s])
+    '''
+        truncated_string = ''               #create empty string truncated_string
         count = 0
-        for char in s:
-            if char == ' ':
+        for char in s:                      #iterate thru characters in string
+            if char == ' ':                 #if character is a space increase counter
                 count += 1
-            if count == k:
+            if count == k:                  #once count = k break the loop
                 break
-            truncated_string += char
+            truncated_string += char        #add character to truncated_string
         return truncated_string
