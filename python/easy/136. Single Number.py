ae32cc6951bc29dca8d93820655c0b058f0c2e2f
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-15 09:36:25 -0500

    Update 136. Single Number.py
    added function comment

--- python/easy/136. Single Number.py
+++ python/easy/136. Single Number.py
@@ -1,5 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+    """
+    singleNumber takes nums List[int] and finds the singular element that is not duplicated and returns it
+    nums List[int] (each element appears twice except for 1)
+        (1 <= nums.length <= 3 * 104)
+        (-3 * 104 <= nums[i] <= 3 * 104)
+    """
         ts_set = set()
         for i in nums:
             if i in ts_set:
