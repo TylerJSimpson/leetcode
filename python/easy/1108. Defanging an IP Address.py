0f66b21b6384bba6812f84c667731dad89e8837f
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-15 09:28:13 -0500

    Update 1108. Defanging an IP Address.py
    Added function comments

--- python/easy/1108. Defanging an IP Address.py
+++ python/easy/1108. Defanging an IP Address.py
@@ -1,4 +1,8 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
+    """
+    defangIPaddr takes in address str (IPv4 address) and replaces the "." with "[.]"
+    address str (is a valid IPv4 address)
+    """
         address_new = address.replace('.','[.]')
         return address_new
