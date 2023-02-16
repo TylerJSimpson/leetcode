e9d0b992f7bef75f28c3e504d247314fb8d2e443
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-02-16 13:08:56 -0500

    Update 1523. Count Odd Numbers in an Interval Range.py
    Added function comment and inline comments

--- python/easy/1523. Count Odd Numbers in an Interval Range.py
+++ python/easy/1523. Count Odd Numbers in an Interval Range.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def countOdds(self, low, high):
-        """
-        :type low: int
-        :type high: int
-        :rtype: int
-        """
-
+    """
+    countOdds takes low (int) and high (int) as inputs and returns the count of odd numbers within the range
+    low: int
+    high: int
+    0 <= low <= high <= 10^9
+    """
         if low % 2 == 1 or high % 2 == 1:
-          return (high-low) / 2 + 1
+          return (high-low) / 2 + 1                 #if low or high are odd divide difference by 2 and add 1
         else:
-          return (high-low) / 2
+          return (high-low) / 2                     #if low and high are even divide difference by 2
