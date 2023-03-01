7ed0988fbeeb51364da2ba465b1a6b878f8b8d81
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-01 18:35:39 -0500

    Update 169. Majority Element.py

--- python/easy/169. Majority Element.py
+++ python/easy/169. Majority Element.py
@@ -1,8 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums_dict = {}
+    """
+    majorityElement returns the element of array nums that appears more than [n / 2] times
+    nums array (n == nums.length) (1 <= n <= 5 * 104) (-109 <= nums[i] <= 109)
+    """
+        nums_dict = {}                                                  #create empty dict
         for number in nums:
-            nums_dict.setdefault(number,0)
-            nums_dict[number] += 1
-        max_number = max(nums_dict, key = lambda x: nums_dict[x])
+            nums_dict.setdefault(number,0)                              #add number in nums to dict set value to 0
+            nums_dict[number] += 1                                      #increment value by 1 for each occurrence of nums
+        max_number = max(nums_dict, key = lambda x: nums_dict[x])       #get key with value of highest increment
         return max_number
