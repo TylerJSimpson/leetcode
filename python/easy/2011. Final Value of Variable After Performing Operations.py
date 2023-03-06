3a94ef0cc83ae88a9a13abf42c9bb0dfa7868411
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-06 18:40:11 -0500

    Update 2011. Final Value of Variable After Performing Operations.py

--- python/easy/2011. Final Value of Variable After Performing Operations.py
+++ python/easy/2011. Final Value of Variable After Performing Operations.py
@@ -1,5 +1,9 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
+    """
+    finalValueAfterOperations returns the same when given 4 increment and decrement possibilities in list operations.
+    ++X and X++ increments, --X and X-- decrements, X begins at 0
+    """
         addition_one = operations.count('X++')
         addition_two = operations.count('++X')
         subtraction_one = operations.count('X--')
