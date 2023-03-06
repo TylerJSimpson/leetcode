7f11624de2fed66828e3daeb7b0efea1dc288e20
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-06 18:43:03 -0500

    Update 9. Palindrome Number.py

--- python/easy/9. Palindrome Number.py
+++ python/easy/9. Palindrome Number.py
@@ -1,10 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        """
-        isPalindrome returns true or false depending on if x int input is a palindrome number
-        palindrome numbers are the same forward as they are backwards
-        x: int (-231 <= x <= 231 - 1)
-        """
+    """
+    isPalindrome returns true or false depending on if x int input is a palindrome number
+    palindrome numbers are the same forward as they are backwards
+    x: int (-231 <= x <= 231 - 1)
+    """
         str_forward = str(x)                            #convert x int to str
         str_backward = str_forward[::-1]                #reverse str
         return str_forward == str_backward              #return comparison of forward and backwards str
