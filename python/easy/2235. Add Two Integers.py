f6ac862aeaa030e1c373b7eb381d4388d59c7873
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-06 18:41:04 -0500

    Update 2235. Add Two Integers.py

--- python/easy/2235. Add Two Integers.py
+++ python/easy/2235. Add Two Integers.py
@@ -1,4 +1,7 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
+    """
+    sum takes num1 and num2 integers and sums them.
+    """
         nums_sum = num1 + num2
         return nums_sum
