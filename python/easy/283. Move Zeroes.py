15bfc499e38d21d46e3a7c6363ca2b8460b48399
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-06 18:42:47 -0500

    Update 283. Move Zeroes.py

--- python/easy/283. Move Zeroes.py
+++ python/easy/283. Move Zeroes.py
@@ -1,7 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        moveZeroes arranges list nums so that all 0s are at the end.
         """
         for i in nums:
             if i == 0:
